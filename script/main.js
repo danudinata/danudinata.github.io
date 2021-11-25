4346f2ff06c982bd9a4385cbb940c7363a1e3850
danudinata <60915916+danudinata@users.noreply.github.com>
2021-11-25 21:02:37 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -135,6 +135,7 @@ const animationTimeline = () => {
       backgroundColor: "rgb(21, 161, 237)",
       color: "#fff",
     })
+    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
     .from(
       ".idea-5",
       0.7, {
